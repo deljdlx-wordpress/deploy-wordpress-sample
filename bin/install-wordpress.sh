d47a83e8c5a40669fd8a9e91a80c24b65e16de30
jdlxt <jdlxt@localhost>
2021-06-01 22:05:04 +0200

    sync

--- bin/install-wordpress.sh
+++ bin/install-wordpress.sh
@@ -1,18 +1,27 @@
 echo "🟢 Project composer install"
 composer install
+
+
+
 if [ -d "vendor/Deljdlx/Deploy" ]; then
-    echo "🟢 Deleting Deljdlx/Deploy vendor"
-    rm -rf ./vendor/Deljdlx/Deploy
+    # echo "🟢 Deleting Deljdlx/Deploy vendor"
+    # rm -rf ./vendor/Deljdlx/Deploy
+    echo "vendor/Deljdlx/Deploy already installed";
+else
+    git clone git@github.com:deljdlx/deploy.git ./vendor/Deljdlx/Deploy;
 fi
 
 if [ -d "vendor/Deljdlx/DeployWordpress" ]; then
-    echo "🟢 Deleting Deljdlx/DeployWordpress vendor"
-    rm -rf ./vendor/Deljdlx/DeployWordpress
+    # echo "🟢 Deleting Deljdlx/DeployWordpress vendor"
+    # rm -rf ./vendor/Deljdlx/DeployWordpress
+    echo "vendor/Deljdlx/DeployWordpress already installed";
+else 
+    git clone git@github.com:deljdlx/deploy-wordpress.git ./vendor/Deljdlx/DeployWordpress;
 fi
 
 
-git clone git@github.com:deljdlx/deploy.git ./vendor/Deljdlx/Deploy
-git clone git@github.com:deljdlx/deploy-wordpress.git ./vendor/Deljdlx/DeployWordpress
+
+
 
 # ========================================================================================
 echo ""
